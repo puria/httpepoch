68b34e8ecc1d3f281e77674b5a012071fa8ffa41
Andrea D'Intino <andrea@tabbles.net>
2021-04-26 10:54:50 +0200

    Updated to not include milliseconds (#1)

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const app = express()
-const port = 3000
+const port = 3312
 
 app.get('/', (req, res) => {
-  res.contentType("text/plain")
-  res.send(new Date().getTime().toString())
+  	res.contentType("text/plain")
+	res.send(`${ (Math.round(Date.now() / 1000)).toString()}`)
+// the one below returns millisecons, which produces a value that doesn't fit 32 bits
+//res.send(`"${new Date().getTime().toString()}"`)
 })
 
 app.listen(port, () => {
